5b4ab13ee5f659ea84c70c7efa66d2b2e211d1f9
hyuliaaa <hulia_99@abv.bg>
2023-03-12 12:41:45 +0200

    Added Spring project

---
+++ src/main/java/com/example/bizzylink/BizzylinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.bizzylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizzylinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizzylinkApplication.class, args);
+    }
+
+}
